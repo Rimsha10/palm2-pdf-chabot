09cf2ae137b90cef2cc6ec7cbddcb5fbdf8b779c
Rimsha <80062949+Rimsha10@users.noreply.github.com>
2023-12-07 20:29:39 +0500

    updated

---
+++ palm2-pdf-chatbot-POC.py
@@ -0,0 +1,79 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import google.generativeai as palm
+from langchain.embeddings import GooglePalmEmbeddings
+from langchain.llms import GooglePalm
+from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferMemory
+import os
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+os.environ['GOOGLE_API_KEY'] =  'AIzaSyAa1FwQZdfl0WrmGY_X_Ni0Fs_NiXg8lko'
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+= page.extract_text()
+    return  text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GooglePalmEmbeddings()
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vector_store
+
+def get_conversational_chain(vector_store):
+  #  context = st.session_state.chatHistory  # Retrieve chat history as context
+     # Define a template for guiding the bot's responses
+
+    # Create a PromptTemplate with the specified template and input variables
+    #prompt = PromptTemplate(template=template, input_variables=["context", "question"])
+
+    llm=GooglePalm(temperature=0, max_tokens=512,system_prompt="You are an assistant helping users with Dell laptop-related issues based on the provided document. "
+        "When a user asks a problem, you provide a relevant solution IN FORM OF BULLET POINTS. "
+        "If the solution doesn't work, offer more explanation")
+    memory = ConversationBufferMemory(memory_key = "chat_history", return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
+
+def user_input(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chatHistory = response['chat_history']
+    for i, message in enumerate(st.session_state.chatHistory):
+        if i%2 == 0:
+            st.write("Human: ", message.content)
+        else:
+            st.write("Bot: ", message.content)
+def main():
+    st.set_page_config("Chat with Multiple PDFs")
+    st.header("Chat with Multiple PDF 💬")
+    user_question = st.text_input("Ask a Question from the PDF Files")
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chatHistory" not in st.session_state:
+        st.session_state.chatHistory = None
+    if user_question:
+        user_input(user_question)
+    with st.sidebar:
+        st.title("Settings")
+        st.subheader("Upload your Documents")
+        pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Process Button", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vector_store = get_vector_store(text_chunks)
+                st.session_state.conversation = get_conversational_chain(vector_store)
+                st.success("Done")
+
+
+
+if __name__ == "__main__":
+    main()
